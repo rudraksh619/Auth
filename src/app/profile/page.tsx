e26fb3ed242a26211f2855727281c20bc1fe9e16
rudraksh619 <rudrakshchandel5@gmail.com>
2025-06-22 14:40:48 +0530

    adding profile_info

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,14 +1,17 @@
 "use client"
 import axios from "axios"
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+
 
 export default  function ShowProfile(){
 
     const router = useRouter();
-    const[user,setuser] = useState();
+    
 
-
+ const getuserdetails = async() =>{
+    const user =  await axios.get('/api/users/Me')
+    console.log("user details" , user.data.data);
+ }
 
   async function getlogout(){
         try {
@@ -25,6 +28,11 @@ export default  function ShowProfile(){
 <div>
     Profile 
 </div>
+<button 
+onClick={getuserdetails}
+className="p-3 rounded-2xl border  hover:bg-pink-400 text-red-600">
+    Get User Details
+</button>
 <button 
 onClick={getlogout}
 className="border p-4 rounded-2xl text-fuchsia-400 bg-black">
